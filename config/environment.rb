a8d6918ee3c7841db050a22aa56e121439b82ef9
Wasif Khan <waasif.khaan@gmail.com>
2018-08-15 22:02:39 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -14,6 +14,7 @@ connection = ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
 )
+
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS artists (
   id INTEGER PRIMARY KEY,
