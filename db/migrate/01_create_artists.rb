301ffb3f7d5aab02ffcd4d59eb682e191ec40b25
Wasif Khan <waasif.khaan@gmail.com>
2018-08-15 22:00:46 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,12 +1,6 @@
 class CreateArtists < ActiveRecord::Migration
-  def up
-  end
- 
-  def down
-  end
-  
   def change
-    create_table :artists do |t|
+      create_table :artists do |t|
       t.string :name
       t.string :genre
       t.integer :age
